c8d4eaeb5823c833153961e527ee3a9791581bcb
ericolson1977 <mr_tues_night@yahoo.com>
2023-10-23 00:16:58 -0500

    started question.js file

---
+++ questions.js
@@ -0,0 +1,10 @@
+const questions = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['View All Departments', 'Add Department', 'Remove Department', 'View All Roles', 'Add Role', 'Remove Role', 'View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View Total Utilized Budget By Department', 'Quit'],
+        name: '',
+    }
+];
+
+module.exports = { questions }
