a0316d1fd0db6d717e03c2a0c55e8a92e1b853e0
ericolson1977 <mr_tues_night@yahoo.com>
2023-10-23 09:51:52 -0500

    redefined id numbers, removed .gitignore and package-clok

--- schema.sql
+++ schema.sql
@@ -4,33 +4,30 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
   department_id INT,
-  PRIMARY KEY (id),
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT,
-  PRIMARY KEY (id),
   FOREIGN KEY (role_id)
   REFERENCES role(id)
   ON DELETE SET NULL,
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
   ON DELETE SET NULL
-);
+) AUTO_INCREMENT = 1001;
--- seeds.sql
+++ seeds.sql
@@ -1,25 +1,25 @@
 INSERT INTO department (id, name)
-VALUES (001, "Pumpkin Carving"),
-       (002, "Apple Picking"),
-       (003, "Pie Making"),
-       (004, "Costume");
+VALUES (1, "Pumpkin Carving"),
+       (2, "Apple Picking"),
+       (3, "Pie Making"),
+       (4, "Costume");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (001, "Sr. Jack o' Lanterner", 85000, 001),
-       (002, "Seed Scooper", 55000, 001),
-       (003, "Executive Apple Selector", 125000, 002),
-       (004, "Bushell Mule", 75000, 002),
-       (005, "Head Cheffy Type", 90000, 003),
-       (006, "Crust Roller", 60000, 003),
-       (007, "Sultan of Sewing", 95000, 004),
-       (008, "Ghost Costumer", 40000, 004);
+VALUES (01, "Sr. Jack o' Lanterner", 85000, 1),
+       (02, "Seed Scooper", 55000, 1),
+       (03, "Executive Apple Selector", 125000, 2),
+       (04, "Bushell Mule", 75000, 2),
+       (05, "Head Cheffy Type", 90000, 3),
+       (06, "Crust Roller", 60000, 3),
+       (07, "Sultan of Sewing", 95000, 4),
+       (08, "Ghost Costumer", 40000, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (001, "Jack", "O'Leary", 001, NULL),
-       (002, "Stanley", "Scoops", 002, 001),
-       (003, "Sally", "SnowSweet", 003, NULL),
-       (004, "Tim", "Carries", 004, 003),
-       (005, "Margot", "Pierre", 005, NULL),
-       (006, "Johnny", "Coldbutter", 006, 005),
-       (007, "Slim", "Bobbins", 007, NULL),
-       (008, "Eric", "Olson", 008, 007);
+VALUES (1001, "Jack", "O'Leary", 01, NULL),
+       (1002, "Stanley", "Scoops", 02, 1001),
+       (1003, "Sally", "SnowSweet", 03, NULL),
+       (1004, "Tim", "Carries", 04, 1003),
+       (1005, "Margot", "Pierre", 05, NULL),
+       (1006, "Johnny", "Coldbutter", 06, 1005),
+       (1007, "Slim", "Bobbins", 07, NULL),
+       (1008, "Eric", "Olson", 08, 1007);
