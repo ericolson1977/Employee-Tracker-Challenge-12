7854cd3cf6479b3e39841c74728fb537751bf26c
ericolson1977 <mr_tues_night@yahoo.com>
2023-10-24 10:13:34 -0500

    removed non-functional code

--- server.js
+++ server.js
@@ -190,58 +190,17 @@ function updateEmployeeRole() {
     });
 }
 
-// app.get('/api/departments', (req, res) => {
-//   db.query('SELECT name AS Department_Name, id AS Department_ID FROM department', (err, results) => {
-//     console.log(results); res.json(results)
-//   })
-// });
-
-// app.get('/api/roles', (req, res) => {
-//   db.query('SELECT title AS Job_Title, role.id AS Role_ID, department.name AS Department, salary AS Salary FROM role JOIN department ON role.department_id = department.id;', (err, results) => {
-//     console.log(results); res.json(results)
-//   })
-// });
-
-// app.get('/api/employees', (req, res) => {
-//   db.query('SELECT e.id AS Employee_ID, e.first_name AS First_Name, e.last_name AS Last_Name, r.title AS Job_Title, d.name AS Department, r.salary AS Salary, m.first_name AS Manager_First_Name, m.last_name AS Manager_Last_Name FROM employee AS e JOIN role AS r ON e.role_id = r.id JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS m ON e.manager_id = m.id; ', (err, results) => {
-//     console.log(results); res.json(results)
-//   })
-// });
-
-// app.post('/api/add-department', (req, res) => {
-//   db.query('INSERT INTO department (name) VALUES (?);', req.body.name, (err, results) => {
-//     console.log(results); res.json(results)
-//   })
-// });
-
-// app.post('/api/add-role', (req, res) => {
-//   db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);', [req.body.title, req.body.salary, req.body.department_id], (err, results) => {
-//     console.log(results); res.json(results)
-//   })
-// });
-
-// app.post('/api/add-employee', (req, res) => {
-//   db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);', [req.body.first_name, req.body.last_name, req.body.role_id, req.body.manager_id], (err, results) => {
-//     console.log(results); res.json(results)
-//   })
-// });
-
-// app.put('/api/role/:id', (req, res) => {
-//   db.query('UPDATE employee SET role_id = ? WHERE id = ?;', [req.body.role_id, req.params.id], (err, results) => {
-//     console.log(results); res.json(results)
-//   })
-// })
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 init();
 
-//why is prompts listed twice
-//remove index columns
-// UI
-// Modularization
+//Why is prompts listed twice (calling init in the functions. Need a .then "Another thing I didnt mention is in order to use .then with the query you need to first make it a promise like
+    //db.promise().query("query")), 
+//Remove index columns  (need to format then date PRIOR to hitting the console.table)
+//UI
+//Modularization
 //Bonus
 //README
-//Video WAlkthrough
+//Video Walkthrough
