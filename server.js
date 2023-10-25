7cf43d24c850cfe9010f1c5b747574ab159764b9
ericolson1977 <mr_tues_night@yahoo.com>
2023-10-24 23:10:51 -0500

    added comments to server.js

--- server.js
+++ server.js
@@ -18,8 +18,10 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_tracker_db database.`)
 );
 
+// asynchronous function to lauch the CMS
 async function init() {
   try {
+    // using await to allow for the user to make a selection
     const answers = await inquirer.prompt([
       {
         type: 'list',
@@ -28,7 +30,7 @@ async function init() {
         name: 'options',
       },
     ]);
-
+    //switch will determine which function to call basedo nthe user's selection
     switch (answers.options) {
       case 'View All Departments':
         viewAllDepartments();
@@ -59,6 +61,7 @@ async function init() {
   };
 };
 
+//pulls department name and id from db and diplays in the terminal
 function viewAllDepartments() {
   db.query('SELECT name AS Department_Name, id AS Department_ID FROM department', (err, results) => {
     if (err) throw err;
@@ -67,6 +70,7 @@ function viewAllDepartments() {
   });
 };
 
+//pulls job role data from db and diplays in the terminal
 function viewAllRoles() {
   db.query('SELECT title AS Job_Title, role.id AS Role_ID, department.name AS Department, salary AS Salary FROM role JOIN department ON role.department_id = department.id;', (err, results) => {
     if (err) throw err;
@@ -75,6 +79,7 @@ function viewAllRoles() {
   });
 };
 
+//pulls employee data from db and diplays in the terminal
 function viewAllEmployees() {
   db.query('SELECT e.id AS Employee_ID, e.first_name AS First_Name, e.last_name AS Last_Name, r.title AS Job_Title, d.name AS Department, r.salary AS Salary, m.first_name AS Manager_First_Name, m.last_name AS Manager_Last_Name FROM employee AS e JOIN role AS r ON e.role_id = r.id JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS m ON e.manager_id = m.id; ', (err, results) => {
     if (err) throw err;
@@ -83,6 +88,7 @@ function viewAllEmployees() {
   });
 };
 
+//takes user input and uses it to add a new department to the database
 function addDepartment() {
   inquirer
     .prompt([
@@ -101,6 +107,7 @@ function addDepartment() {
     });
 };
 
+//grabs department info from db in order to use it in an inquirer list prompt
 function getDepartmentChoices(callback) {
   db.query('SELECT name AS Department_Name, id AS Department_ID FROM department', (err, results) => {
     if (err) throw err;
@@ -112,6 +119,7 @@ function getDepartmentChoices(callback) {
 });
 };
 
+//grabs job role info from db in order to use it in an inquirer list prompt
 function getRoles(callback) {
   db.query('SELECT id, title FROM role', (err, roles) => {
     if (err) throw err;
@@ -120,6 +128,7 @@ function getRoles(callback) {
   });
 };
 
+//grabs employee info from db in order to use it in an inquirer list prompt
 function getEmployees(callback) {
   db.query('SELECT id, first_name, last_name FROM employee', (err, employees) => {
     if (err) throw err;
@@ -128,6 +137,7 @@ function getEmployees(callback) {
   });
 };
 
+//takes user input and uses it to add a new job role to the db
 function addRole() {
   getDepartmentChoices((departmentChoices) => {
     inquirer
@@ -160,6 +170,7 @@ function addRole() {
     });
 };
 
+//takes user input and uses it to add a new employee to the db
 function addEmployee() {
   getRoles((roleChoices) => {
     getEmployees((employeeChoices) => {
@@ -185,6 +196,7 @@ function addEmployee() {
         type: 'list',
         name: 'manager',
         message: "Enter the employee's manager (if applicable):",
+        //allows for null entry indicating this employee has no manager and therefore is a manager
         choices: [...employeeChoices, {name:'None', value: null }]
       }
     ])
@@ -202,6 +214,7 @@ function addEmployee() {
   });
 };
 
+//take user input and uses it to update an employee job role and sends to the db
 function updateEmployeeRole() {
   getRoles((roleChoices) => {
     getEmployees((employeeChoices) => {
@@ -231,11 +244,10 @@ function updateEmployeeRole() {
 });
 };
 
+//verifies connection to server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+//function call to start app
 init();
-
-//README
-//Video Walkthrough
